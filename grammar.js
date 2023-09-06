051adc57dbad7f708c8a04829f5aab6983abdbf2
William Hleucka <william.hleucka@gmail.com>
2023-09-06 14:04:20 -0600

    working on corpus

--- grammar.js
+++ grammar.js
@@ -1,27 +1,61 @@
-const REGEX_NAME = /[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*/;
-
 module.exports = grammar({
   name: 'latte',
 
   rules: {
     template: $ => repeat(
-      choice($.layout_tag, $.block_tag)
+      choice($.include_tag, $.layout_tag, $.comment, $.block_tag, $.html_tag, $.expression_tag, $.text)
     ),
 
     layout_tag: $ => seq(
       '{layout',
-      $.layout_path,
+      $.file_path,
       '}'
     ),
 
+    file_path: $ => /[^\}]+/,
+
     block_tag: $ => seq(
       '{block',
-      optional($.content),
+      $.block_name,
+      '}',
+      repeat($.block_content),
       '{/block}'
     ),
 
-    layout_path: $ => /[^\}]+/,
-    _name: () => REGEX_NAME,
-    content: $ => /[^{]+/
+    block_name: $ => /[^\}]+/,
+    block_content: $ => choice($.text, $.layout_tag, $.comment, $.html_tag, $.expression_tag),
+
+    include_tag: $ => seq(
+      '{include',
+      $.include_file_path,
+      ',',
+      $.include_options,
+      '}'
+    ),
+
+    include_file_path: $ => /[^,]+/,
+    include_options: $ => /[^}]+/,
+
+    expression_tag: $ => seq(
+      '{',
+      $.expression_name,
+      '}'
+    ),
+    expression_name: $ => /[^\}]+/,
+
+    comment: () => seq('{*', /[^#]*\#+([^\}#][^#]*\#+)*/, '*}'),
+
+    html_tag: $ => seq(
+      '<',
+      /[^\{\}>]+/, // Match HTML tag name (excluding '{', '}', and '>')
+      '>',
+      repeat(choice($.text, $.expression_tag)), // Allow text and expressions within the tag
+      '</',
+      /[^\{\}>]+/, // Match closing tag name (excluding '{', '}', and '>')
+      '>'
+    ),
+
+    text: $ => /[^{}<]+/, // Match any text that is not inside curly braces or HTML tags
   }
 });
+
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,6 +1,16 @@
+(comment) @comment
+(include_tag) @keyword
+(include_file_path) @string
+(include_options) @string
 (layout_tag) @keyword
+(file_path) @string
 (block_tag) @keyword
+(block_name) @attribute
+(expression_tag) @keyword
+(expression_name) @attribute
 
-(layout_path) @string
+[
+    "{"
+    "}"
+] @tag.delimiter
 
-(content) @text
